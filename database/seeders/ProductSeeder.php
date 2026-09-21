cf19d51bde53d82205b504f3091f35d63464c14e
Nanang Ibnu Qosim <devibnuq@gmail.com>
2026-09-21 20:56:49 +0700

    refactor: make product seeder environment-safe

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -6,7 +6,10 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Product;
 use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Application;
 use Illuminate\Support\Str;
+use RuntimeException;
 
 class ProductSeeder extends Seeder
 {
@@ -18,9 +21,8 @@ class ProductSeeder extends Seeder
         $products = [
             [
                 'name' => 'GS Astra NS40Z',
-
+                'category_slug' => 'aki-mobil',
                 'official_brand' => 'GS Astra',
-                'category_id' => 1, // Aki Mobil
                 'voltage' => '12V',
                 'capacity_ah' => 35,
                 'cca' => 320,
@@ -28,13 +30,12 @@ class ProductSeeder extends Seeder
                 'price' => 750000,
                 'is_price_visible' => true,
                 'is_active' => true,
-                'applications' => [1], // Otomotif
+                'application_slugs' => ['otomotif'],
             ],
             [
                 'name' => 'Incoe Gold N200',
-
+                'category_slug' => 'aki-truk-bus',
                 'official_brand' => 'Incoe',
-                'category_id' => 2, // Aki Truk & Bus
                 'voltage' => '12V',
                 'capacity_ah' => 200,
                 'cca' => 1100,
@@ -42,13 +43,12 @@ class ProductSeeder extends Seeder
                 'price' => 2350000,
                 'is_price_visible' => true,
                 'is_active' => true,
-                'applications' => [2, 3], // Truk & Bus, Alat Berat
+                'application_slugs' => ['truk-dan-bus', 'alat-berat'],
             ],
             [
                 'name' => 'Yuasa Pafecta N120',
-
+                'category_slug' => 'aki-marine',
                 'official_brand' => 'Yuasa',
-                'category_id' => 4, // Aki Kapal
                 'voltage' => '12V',
                 'capacity_ah' => 120,
                 'cca' => 800,
@@ -56,13 +56,12 @@ class ProductSeeder extends Seeder
                 'price' => 1700000,
                 'is_price_visible' => false,
                 'is_active' => true,
-                'applications' => [4, 5], // Marine/Kapal, Genset
+                'application_slugs' => ['marine-kapal', 'genset'],
             ],
             [
                 'name' => 'Amaron Quanta 12V 100Ah',
-
+                'category_slug' => 'aki-industri-deep-cycle',
                 'official_brand' => 'Amaron',
-                'category_id' => 6, // Aki UPS
                 'voltage' => '12V',
                 'capacity_ah' => 100,
                 'cca' => null,
@@ -70,13 +69,12 @@ class ProductSeeder extends Seeder
                 'price' => 2100000,
                 'is_price_visible' => true,
                 'is_active' => true,
-                'applications' => [8, 9], // Telekomunikasi, UPS Data Center
+                'application_slugs' => ['telekomunikasi', 'ups-data-center'],
             ],
             [
                 'name' => 'Massiv Amal N70Z',
-
+                'category_slug' => 'aki-alat-berat',
                 'official_brand' => 'Yuasa',
-                'category_id' => 3, // Aki Alat Berat
                 'voltage' => '12V',
                 'capacity_ah' => 75,
                 'cca' => 550,
@@ -84,17 +82,28 @@ class ProductSeeder extends Seeder
                 'price' => 1150000,
                 'is_price_visible' => true,
                 'is_active' => true,
-                'applications' => [3, 6], // Alat Berat, Forklift
+                'application_slugs' => ['alat-berat', 'forklift'],
             ]
         ];
 
         foreach ($products as $data) {
-            $apps = $data['applications'];
-            unset($data['applications']);
-            
-            $officialBrand = Brand::where('name', $data['official_brand'])->firstOrFail();
-            $data['brand_id'] = $officialBrand->id;
+            $category = Category::where('slug', $data['category_slug'])->firstOrFail();
+            $brand = Brand::where('name', $data['official_brand'])->firstOrFail();
+
+            $applications = Application::whereIn('slug', $data['application_slugs'])->pluck('id');
+
+            if ($applications->count() !== count($data['application_slugs'])) {
+                throw new RuntimeException("One or more applications could not be resolved for product: {$data['name']}");
+            }
+
+            $data['category_id'] = $category->id;
+            $data['brand_id'] = $brand->id;
+
+            $apps = $applications->toArray();
+
+            unset($data['category_slug']);
             unset($data['official_brand']);
+            unset($data['application_slugs']);
             
             $data['slug'] = Str::slug($data['name']);
             
